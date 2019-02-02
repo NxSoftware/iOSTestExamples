19f5f1e7d5417f4f89904b0ee02ad329810e533d
Steve Wilford <steve@stevewilford.co.uk>
2019-02-02 11:36:25 +0000

    initial commit

---
+++ iOSTestExamples/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  iOSTestExamples
+//
+//  Created by Steve Wilford on 02/02/2019.
+//  Copyright © 2019 Off Topic. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ iOSTestExamplesTests/iOSTestExamplesTests.swift
@@ -0,0 +1,22 @@
+//
+//  iOSTestExamplesTests.swift
+//  iOSTestExamplesTests
+//
+//  Created by Steve Wilford on 02/02/2019.
+//  Copyright © 2019 Off Topic. All rights reserved.
+//
+
+import XCTest
+@testable import iOSTestExamples
+
+class iOSTestExamplesTests: XCTestCase {
+
+    func testPasses() {
+        XCTAssertTrue(true)
+    }
+    
+    func testFails() {
+        XCTAssertTrue(false)
+    }
+
+}
